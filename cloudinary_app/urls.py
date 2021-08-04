279b9c344d6037f14702ce1046298a8fe63d2e3c
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2021-08-04 14:06:30 -0300

    Added views effect, get categorized and upload categorized

--- cloudinary_app/urls.py
+++ cloudinary_app/urls.py
@@ -3,5 +3,7 @@ from cloudinary_app.views import *
 from django.urls import path
 
 urlpatterns = [
-    path('get-all-imgs/<str:folder>/', get_all_imgs, name="get_all_imgs")
+    path('get-all-imgs/<str:folder>/', get_all_imgs, name="get_all_imgs"),
+    path('upload-categorized-img/<str:folder>/<str:id>/', upload_categorized_img, name="upload_categorized_img"),
+    path('get-img-with-effect/<str:folder>/<str:id>/', get_img_with_effect, name="download_img_with_effect")
 ]
--- cloudinary_app/views.py
+++ cloudinary_app/views.py
@@ -5,7 +5,9 @@ from cloudinary_app.serializers import MyImageSerializer
 
 from rest_framework.response import Response
 
+import cloudinary as cd
 import cloudinary.api as ca
+import cloudinary.uploader as up
 # Create your views here.
 
 @api_view(["GET"])
@@ -18,3 +20,42 @@ def get_all_imgs(request, *args, **kwargs):
     for i in data["resources"]:
         images.append(MyImageSerializer(MyImage(i["url"], i["public_id"], i["width"], i["height"], i["bytes"] // 1024)).data)
     return Response(images)
+
+@api_view(["POST"])
+def upload_categorized_img(request, *args, **kwargs):
+
+    if (request.FILES.get('img', None) is not None):
+        imgFile = request.FILES['img']
+        img = up.upload(
+            imgFile,
+            folder = kwargs["folder"],
+            public_id = kwargs["id"],
+            overwrite = True,
+            categorization = "google_tagging",
+            auto_tagging = 0.6
+        )
+        return Response(MyImageSerializer(MyImage(img["url"], img["public_id"], img["width"], img["height"], img["bytes"] // 1024)).data)
+
+    return Response("Nenhum arquivo enviado para o cloudinary!")
+
+
+@api_view(["GET"])
+def get_img_with_effect(request, *args, **kwargs):
+    img = cd.CloudinaryImage(f"{kwargs['folder']}/{kwargs['id']}").image(transformation=[
+        {'aspect_ratio': "1.0", 'gravity': "face", 'width': "0.6", 'zoom': "0.7", 'crop': "thumb"},
+        {'radius': "max"},
+        {'color': "brown", 'effect': "outline"},
+        {'color': "grey", 'effect': "shadow", 'x': 30, 'y': 55}
+    ])
+    return Response(img[10:-3])
+
+@api_view(["GET"])
+def get_categorized_img(request, *args, **kwargs):
+    dict = cd.Search().expression(request.GET.get("tag")).execute()
+    images = []
+    for _, v in dict.items():
+        if isinstance(v, list):
+            for img in v:
+                images.append(MyImageSerializer(MyImage(img["url"], img["public_id"], img["width"], img["height"], img["bytes"] // 1024)).data)
+    return Response(images)
+
